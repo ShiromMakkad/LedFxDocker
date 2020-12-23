913d722c92309c096ac909604c875b7625831d2b
Shirom Makkad <shirmak11@gmail.com>
2020-12-22 21:09:59 -0600

    Changed Snapclient to Snapserver for ARM

--- setup-files/snapcast.sh
+++ setup-files/snapcast.sh
@@ -3,8 +3,8 @@
 cd /app
 
 if [[ $(dpkg --print-architecture) ==  "armhf" ]] || [[ $(dpkg --print-architecture) == "arm64" ]]; then
-    wget https://github.com/badaix/snapcast/releases/download/v0.22.0/snapserver_0.22.0-1_armhf.deb
-    dpkg -i snapserver_0.22.0-1_armhf.deb
+    wget https://github.com/badaix/snapcast/releases/download/v0.22.0/snapclient_0.22.0-1_armhf.deb
+    dpkg -i snapclient_0.22.0-1_armhf.deb
 else
     wget https://github.com/badaix/snapcast/releases/download/v0.22.0/snapclient_0.22.0-1_amd64.deb
     dpkg -i snapclient_0.22.0-1_amd64.deb
