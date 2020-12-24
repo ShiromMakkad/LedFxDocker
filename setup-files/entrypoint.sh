08d29ba7073459777cd1d1bb3426d7ec37bd3ca9
Shirom Makkad <shirmak11@gmail.com>
2020-12-24 04:08:46 -0600

    Added optional input type based on env vars

--- setup-files/entrypoint.sh
+++ setup-files/entrypoint.sh
@@ -1,4 +1,16 @@
-./pipe-audio.sh
-./snapcast.sh
+#!/bin/bash
+
+# https://superuser.com/questions/1539634/pulseaudio-daemon-wont-start-inside-docker
+# Start the pulseaudio server
+rm -rf /var/run/pulse /var/lib/pulse /root/.config/pulse
+pulseaudio -D --verbose --exit-idle-time=-1 --system --disallow-exit
+
+if [[ -v FORMAT ]]; then
+    ./pipe-audio.sh
+fi
+
+if [[ -v HOST ]]; then
+    ./snapcast.sh
+fi
 
 ledfx
--- setup-files/pipe-audio.sh
+++ setup-files/pipe-audio.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-# https://superuser.com/questions/1539634/pulseaudio-daemon-wont-start-inside-docker
-rm -rf /var/run/pulse /var/lib/pulse /root/.config/pulse
-pulseaudio -D --verbose --exit-idle-time=-1 --system --disallow-exit
-
 rm /app/audio/stream
 mkfifo -m a+rw /app/audio/stream
 while true; do cat /app/audio/stream | aplay $FORMAT; done &
