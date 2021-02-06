55f1d70a6d7fb4302f8b480b03502c3f742c58bd
Vanixxx <43546061+Vanixxx@users.noreply.github.com>
2021-02-06 15:15:04 +0100

    Update squeeze.sh

--- setup-files/squeeze.sh
+++ setup-files/squeeze.sh
@@ -1,5 +1,5 @@
 if [ "$SQUEEZE" == "1" ]; then
       apt-get install -fy squeezelite
       cp -v /app/ledfx-config/ /etc/default/squeezelite
-      squeezelite -z
+      /etc/init.d/squeezelite start
 fi
